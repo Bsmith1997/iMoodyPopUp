eeb66e67ae62dd9f29435d4dc09c60b77e89e364
Babette Smith <babette.smith@mail.mcgill.ca>
2019-11-22 17:59:47 -0500

    Adding spring backend

---
+++ iMoodyPopUp-Backend/src/test/java/iMoodyPopUp/IMoodyPopUpApplicationTests.java
@@ -0,0 +1,13 @@
+package iMoodyPopUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMoodyPopUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
