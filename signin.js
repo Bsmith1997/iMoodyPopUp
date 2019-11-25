c8b58597a793e61253e23f9ac8ec0cc5dbe1133d
Sam Cleland <edward.cleland@mail.mcgill.ca>
2019-11-25 17:34:53 -0500

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,1 +1,22 @@
 document.getElementById("back_button").addEventListener("click", signout);
+document.getElementById("myForm").addEventListener("submit", function() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://ghibliapi.herokuapp.com/films', false)
+    request.onload = function() {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+    
+      if (request.status >= 200 && request.status < 400) {
+        data.forEach(movie => {
+          console.log(movie.title)
+        })
+
+      } else {
+        console.log('error')
+      }
+    }
+    
+    request.send()
+    account_page();
+})
