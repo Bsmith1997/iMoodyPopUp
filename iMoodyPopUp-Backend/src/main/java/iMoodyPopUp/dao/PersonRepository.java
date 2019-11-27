42cb0d05d5853a1e833f41344aaeb1613c7099ee
Sam Cleland <edward.cleland@mail.mcgill.ca>
2019-11-27 16:54:40 -0500

    Fix backend

--- iMoodyPopUp-Backend/src/main/java/iMoodyPopUp/dao/PersonRepository.java
+++ iMoodyPopUp-Backend/src/main/java/iMoodyPopUp/dao/PersonRepository.java
@@ -1,14 +1,11 @@
 package iMoodyPopUp.dao;
 
-import javax.persistence.Entity;
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import iMoodyPopUp.model.Person;
 
 public interface PersonRepository extends CrudRepository<Person, String>{
 
-    Person findPersonByName(String name);
+    Person findPersonByEmail(String email);
 
 }
